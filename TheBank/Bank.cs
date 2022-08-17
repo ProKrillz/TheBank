ee92588964299a04a80e0e7843b41c8a65dcecff
ProKrillz <thomasdamkjaer@gmail.com>
2022-08-17 10:24:17 +0200

    Add project files.

---
+++ TheBank/Bank.cs
@@ -0,0 +1,32 @@
+﻿namespace TheBank
+{
+    public class Bank
+    {
+        public string _bankName;
+        int _idCounter;
+        List<Account> _accounts;
+
+        public Bank(string name)
+        {
+            _bankName = name;
+            _accounts = new List<Account>();
+        }
+        public void CreateAccount(string name)
+        {
+            _accounts.Add(new Account { Id = _idCounter++, Name = name, Balance = 0 });
+        }
+        public List<Account> GetAccounts() { return _accounts; }
+        public decimal Deposit(Account account, decimal money)
+        {
+            account.Balance += money;
+            return account.Balance;
+        }
+        public decimal Withdraw(Account account, decimal money)
+        {
+            account.Balance -= money;
+            return account.Balance;
+        }
+        public decimal Balance(Account account) { return account.Balance; }
+
+    }
+}
---
+++ TheBank/Program.cs
@@ -0,0 +1,13 @@
+﻿using TheBank;
+
+Bank theBank = new Bank("Krillz Bank Of Doom");
+theBank.CreateAccount("Brian");
+List <Account> test = theBank.GetAccounts();
+Account Account = test[0];
+Console.WriteLine(theBank._bankName);
+Console.WriteLine($"Acount created {Account.Name} and balance is {Account.Balance} kr.");
+Console.WriteLine($"Deposit: 300 kr. Balance: {theBank.Deposit(Account, 300)} kr.");
+Console.WriteLine($"Witdraw: 100 kr. Balance: {theBank.Withdraw(Account, 100)} kr.");
+Console.WriteLine($"Balance: {theBank.Balance(Account)} kr.");
+
+

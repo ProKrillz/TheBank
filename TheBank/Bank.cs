8fe5679392bdad1a92c5dcbc3ed88ac115b06ddd
ProKrillz <thomasdamkjaer@gmail.com>
2022-08-17 13:12:45 +0200

    Bank 3 plus summary

--- TheBank/Bank.cs
+++ TheBank/Bank.cs
@@ -11,23 +11,65 @@
             _bankName = name;
             _accounts = new List<Account>();
         }
+        /// <summary>
+        /// Creates a user with name and count id with 1 and add to accountlist
+        /// </summary>
+        /// <param name="name"></param>
         public void CreateAccount(string name)
         {
             _accounts.Add(new Account { Id = ++_idCounter, Name = name, Balance = 0 });
         }
+        /// <summary>
+        /// Returns bank name
+        /// </summary>
+        /// <returns>string</returns>
         public string GetBankName() { return _bankName; }
+        /// <summary>
+        /// Returns all accounts in a list
+        /// </summary>
+        /// <returns>List</returns>
         public List<Account> GetAccounts() { return _accounts; }
+        /// <summary>
+        /// Add money to account balance
+        /// </summary>
+        /// <param name="account"></param>
+        /// <param name="money"></param>
+        /// <returns>decimal</returns>
         public decimal Deposit(Account account, decimal money)
         {
             account.Balance += money;
             return account.Balance;
         }
+        /// <summary>
+        /// minus money from account balance
+        /// </summary>
+        /// <param name="account"></param>
+        /// <param name="money"></param>
+        /// <returns>decimal</returns>
         public decimal Withdraw(Account account, decimal money)
         {
             account.Balance -= money;
             return account.Balance;
         }
+        /// <summary>
+        /// Returns account balance
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns>decimal</returns>
         public decimal Balance(Account account) { return account.Balance; }
+        /// <summary>
+        /// pluses every diffrent account balance
+        /// </summary>
+        /// <returns>decimal</returns>
+        public decimal BankHolding()
+        {
+            decimal bankhold = 0;
+            foreach (Account item in _accounts)
+            {
+                bankhold += item.Balance;
+            }
+            return bankhold;
+        }
 
     }
 }
--- TheBank/Program.cs
+++ TheBank/Program.cs
@@ -5,43 +5,59 @@
         public static void Main()
         {
             Bank theBank = new Bank("Krillz Bank Of Doom");
+            bool runTime = true;
             do
             {
+                Console.Clear();
                 Console.WriteLine(theBank.GetBankName());
                 Menu();
                 switch (InputString("Choise: "))
                 {
                     case "a":
                         theBank.CreateAccount(InputString("Name: "));
+                        Console.ReadKey();
                         break;
                     case "d":
                         PrintListUsers(theBank.GetAccounts());
                         int accountId = InputInt("Pick id: ");
                         Account foundAccount = theBank.GetAccounts().Find(x => x.Id == accountId);
-                        Console.WriteLine(theBank.Deposit(foundAccount, InputDecimal("Deposit amount: "))); 
+                        Console.WriteLine(theBank.Deposit(foundAccount, InputDecimal("Deposit amount: ")));
+                        Console.ReadKey();
                         break;
                     case "w":
                         PrintListUsers(theBank.GetAccounts());
                         int accountIdW = InputInt("Pick id: ");
                         Account foundAccountW = theBank.GetAccounts().Find(x => x.Id == accountIdW);
                         Console.WriteLine(theBank.Withdraw(foundAccountW, InputDecimal("Deposit amount: ")));
+                        Console.ReadKey();
                         break;
                     case "s":
                         PrintListUsers(theBank.GetAccounts());
                         int accountIdS = InputInt("Pick id: ");
                         Account foundAccountS = theBank.GetAccounts().Find(x => x.Id == accountIdS);
                         Console.WriteLine($"{foundAccountS.Name} Balance: {theBank.Balance(foundAccountS)}");
+                        Console.ReadKey();
+                        break;
+                    case "h":
+                        Console.WriteLine($"Bank holding: {theBank.BankHolding()}");
+                        Console.ReadKey();
                         break;
                     case "b":
                         Console.WriteLine($"Bank name: {theBank.GetBankName()}");
+                        Console.ReadKey();
+                        break;
+                    case "x":
+                        runTime = false;
                         break;
                     default:
                         break;
                 }
 
-            } while (true);
+            } while (runTime);
         }
-
+        /// <summary>
+        /// Show menu
+        /// </summary>
         public static void Menu() 
         {  
             Console.WriteLine("Menu");
@@ -49,8 +65,14 @@
             Console.WriteLine("d = Deposit");
             Console.WriteLine("w = Witdraw");
             Console.WriteLine("s = Show balance");
+            Console.WriteLine("h = BankHolding");
             Console.WriteLine("b = Bank");
+            Console.WriteLine("x = Exit");
         }
+        /// <summary>
+        /// Show account id and name from accountlist
+        /// </summary>
+        /// <param name="list"></param>
         public static void PrintListUsers(List<Account> list)
         {
             foreach (Account account in list)
@@ -60,6 +82,11 @@
                 Console.WriteLine();
             }
         }
+        /// <summary>
+        /// Writeline for returning only string
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>string</returns>
         static string InputString(string text)
         {
             while (true)
@@ -70,6 +97,11 @@
                     return indput;
             }
         }
+        /// <summary>
+        /// Writeline for returning only int
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>int</returns>
         static int InputInt(string text)
         {
             int value;
@@ -86,6 +118,11 @@
                 }
             }
         }
+        /// <summary>
+        /// Writeline for returning only decimal
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>decimal</returns>
         static decimal InputDecimal(string text)
         {
             decimal value;
